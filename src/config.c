c29fa92b4913a496d65d70b4799ea3ef19ece7e1
akash0708 <akashbag12a05@gmail.com>
2025-01-27 11:39:31 +0530

    upto setadvparsing

--- src/config.c
+++ src/config.c
@@ -106,15 +106,28 @@ napi_value CreateConfig(napi_env env, napi_callback_info info)
 // Wrapper for pinggy_config_set_server_address
 napi_value ConfigSetServerAddress(napi_env env, napi_callback_info info)
 {
-    size_t argc = 2;
-    napi_value args[2];
-    napi_value result;
+    // env: the Node-API environment pointer
+    // info: contains information about the function call including arguments
+
+    size_t argc = 2;    // Number of expected arguments
+    napi_value args[2]; // Array to store the JS arguments
+    napi_value result;  // return value for JS
 
     // Parse arguments
     napi_get_cb_info(env, info, &argc, args, NULL, NULL);
+    // napi_status napi_get_cb_info(napi_env env,
+    //                          napi_callback_info cbinfo,
+    //                          size_t* argc,
+    //                          napi_value* argv,
+    //                          napi_value* thisArg,
+    //                          void** data)
+    // [in] env: the Node-API environment
+    // [in] cbinfo: the callback info passed into the callback function
+    // [in-out] argc: specifies the length of the provided 'argv' array and receives the actual count of arguments. argc can optionally be ignored by passing NULL
+    // [out] argv: C array of napi_values to which the arguments will be copied. If there are more arguments then the provided count, only the requested no of arguments are copied. If there are fewer arguments provided than claimed, the rest of argv is filled with napi_value vales that represent 'undefined'. 'argv' can also be optionally ignored.
 
     // Validate the number of arguments
-    if (argc < 2)
+    if (argc < 2) // check the received number of arguments
     {
         napi_throw_type_error(env, NULL, "Expected two arguments (config, server_address)");
         return NULL;
@@ -123,12 +136,25 @@ napi_value ConfigSetServerAddress(napi_env env, napi_callback_info info)
     // Get the first argument: config (uint32_t)
     uint32_t config;
     napi_get_value_uint32(env, args[0], &config);
+    // napi_status napi_get_value_uint32(napi_env env,
+    //                               napi_value value,
+    //                               uint32_t* result)
+    // [in] env: the Node-API environment
+    // [in] value: the JavaScript number to convert
+    // [out] result: C primitive equivalent of the given napi_value as a uint32_t
+
+    // the conversion is needed because
+    // - In JavaScript, all numbers (whether integer or floating-point) are represented as double-precision floating-point numbers.
+    // - In C, we have distinct types for integers (int, uint32_t, etc.) and floating-point numbers (float, double).
 
     // Get the second argument: server_address (string)
     size_t str_len;
     napi_get_value_string_utf8(env, args[1], NULL, 0, &str_len);
+    // returns only the length of the string excluding the null terminator in `result`
+
     char *server_address = (char *)malloc(str_len + 1); // Allocate memory for the C string
     napi_get_value_string_utf8(env, args[1], server_address, str_len + 1, NULL);
+    // copies the string into the buffer
 
     // Call the pinggy_config_set_server_address function
     pinggy_config_set_server_address(config, server_address);
@@ -144,15 +170,27 @@ napi_value ConfigSetServerAddress(napi_env env, napi_callback_info info)
 // Wrapper for pinggy_config_get_server_address
 napi_value ConfigGetServerAddress(napi_env env, napi_callback_info info)
 {
-    size_t argc = 1;
-    napi_value args[1];
-    napi_value result;
+    // env: the Node-API environment pointer
+    // info: contains information about the function call including arguments
+
+    size_t argc = 1;   // Number of expected arguments
+    napi_value args[1]; // Array to store the JS arguments (passed to cb_info function)
+    napi_value result; // return value for JS
 
     // Parse arguments
     napi_get_cb_info(env, info, &argc, args, NULL, NULL);
+    // napi_status napi_get_cb_info(napi_env env,
+    //                          napi_callback_info cbinfo,
+    //                          size_t* argc,
+    //                          napi_value* argv,
+    //                          napi_value* thisArg,
+    //                          void** data)
+    // [in] env: the Node-API environment
+    // [in] cbinfo: the callback info passed into the callback function
+    // [in-out] argc: specifies number of expected parameters and receives the actual count of arguments. argc can optionally be ignored by passing NULL
 
     // Validate the number of arguments
-    if (argc < 1)
+    if (argc < 1) // check the received number of arguments
     {
         napi_throw_type_error(env, NULL, "Expected one argument (config)");
         return NULL;
@@ -161,13 +199,20 @@ napi_value ConfigGetServerAddress(napi_env env, napi_callback_info info)
     // Get the first argument: config (uint32_t)
     uint32_t config;
     napi_get_value_uint32(env, args[0], &config);
+    // napi_status napi_get_value_uint32(napi_env env,
+    //                               napi_value value,
+    //                               uint32_t* result)
+    // [in] env: the Node-API environment
+    // [in] value: the JavaScript number to convert, received as the first argument
+    // [out] result: C primitive equivalent of the given napi_value as a uint32_t, since JS numbers are to double type
 
     // Allocate buffer for the server address
-    pinggy_capa_t buffer_len = 256; // Example buffer length
-    char buffer[256];               // Fixed-size character array for simplicity
+    pinggy_capa_t buffer_len = 512; // Example buffer length
+    char buffer[512];               // Fixed-size character array for simplicity
 
     // Call the pinggy_config_get_server_address function
     int copied_len = pinggy_config_get_server_address(config, buffer_len, buffer);
+    // get the length from the JS function preferabbly as a parameter 
 
     // Handle errors (if the copied length is negative, assuming it indicates an error)
     if (copied_len < 0)
@@ -178,18 +223,35 @@ napi_value ConfigGetServerAddress(napi_env env, napi_callback_info info)
 
     // Convert the buffer to a JavaScript string
     napi_create_string_utf8(env, buffer, copied_len, &result);
+    // napi_status napi_create_string_utf8(napi_env env,
+    //                                  const char* str,
+    //                                  size_t length,
+    //                                  napi_value* result)
+    // [in] env: the Node-API environment
+    // [in] str: Character buffer representing a UTF-8 encoded string
+    // [in] length: the length of the string in bytes, or NAPI_AUTO_LENGTH if the string is null-terminated
+    // [out] result: the JavaScript string value
     return result;
 }
 
 // Wrapper for pinggy_config_get_sni_server_name
 napi_value ConfigGetSniServerName(napi_env env, napi_callback_info info)
 {
-    size_t argc = 1;
-    napi_value args[1];
-    napi_value result;
+    // env: the Node-API environment pointer
+    // info: contains information about the function call including arguments
+
+    size_t argc = 1; // Number of expected arguments
+    napi_value args[1]; // Array to store the JS arguments (passed to cb_info function)
+    napi_value result; // return value for JS
 
     // Parse arguments
     napi_get_cb_info(env, info, &argc, args, NULL, NULL);
+    // [in] env: the Node-api environment
+    // [in] info: the callback info passed into the callback function
+    // [in-out] argc: specifies number of expected parameters and recieves the actual count of arguments. argc can optionally be ignored by passing NULL
+    // [out] args: C array of napi_values to which the arguments will be copied. If there are more arguments than the provided count, only the requested no of arguments are copied. If ther are fewer arguments provided than claimed, the rest of args is filled with napi_value values that represent 'undefined'. 'args' can also be optionally ignored.
+    // [out] NULL: thisArg, the JavaScript 'this' argument for the call. Ignored in this case by passing NULL
+    // [out] NULL: the pointer to store the number of bytes copied, which we are not interested in here.
 
     // Validate the number of arguments
     if (argc < 1)
@@ -203,8 +265,8 @@ napi_value ConfigGetSniServerName(napi_env env, napi_callback_info info)
     napi_get_value_uint32(env, args[0], &config);
 
     // Allocate buffer for the SNI server name
-    pinggy_capa_t buffer_len = 256; // Example buffer length
-    char buffer[256];               // Fixed-size character array for simplicity
+    pinggy_capa_t buffer_len = 512; // Example buffer length
+    char buffer[512];               // Fixed-size character array for simplicity
 
     // Call the pinggy_config_get_sni_server_name function
     int copied_len = pinggy_config_get_sni_server_name(config, buffer_len, buffer);
@@ -241,11 +303,13 @@ napi_value ConfigSetSniServerName(napi_env env, napi_callback_info info)
     // Get the first argument: config (uint32_t)
     uint32_t config;
     napi_get_value_uint32(env, args[0], &config);
+    // convert the JS number(which of double tyep) to C uint32_t
 
     // Get the second argument: sni_server_name (string)
     size_t str_read;
-    char sni_server_name[256]; // Buffer for the SNI server name
+    char sni_server_name[512]; // Buffer for the SNI server name
     napi_get_value_string_utf8(env, args[1], sni_server_name, sizeof(sni_server_name), &str_read);
+    // convert the JS string to C string
 
     // Call the pinggy_config_set_sni_server_name function
     pinggy_config_set_sni_server_name(config, sni_server_name);
@@ -254,6 +318,8 @@ napi_value ConfigSetSniServerName(napi_env env, napi_callback_info info)
     napi_get_undefined(env, &result);
     return result;
 }
+
+// ------------------ resume from here ------------------
 
 // Wrapper for pinggy_config_set_advanced_parsing
 napi_value ConfigSetAdvancedParsing(napi_env env, napi_callback_info info)
