6ec39b89d9272125e1a03074ee54496c3a8c55b3
akash0708 <akashbag12a05@gmail.com>
2025-06-06 11:34:06 +0530

    resolved merge conflicts, functioning properly for linux now

--- copy.js
+++ copy.js
@@ -3,9 +3,7 @@ const path = require("path");
 
 const src = path.join(__dirname, "build", "Release", "addon.node");
 const dest = path.join(__dirname, "lib", "addon.node");
-const libPathWin = path.join(__dirname, "pinggy.lib"); // <- path to pinggy.lib
-const libPathMac = path.join(__dirname, "libpinggy.dylib"); // <- path to libpinggy.dylib
-const libPathLinux = path.join(__dirname, "libpinggy.so"); // <- path to libpinggy.so
+const libPath = path.join(__dirname, "pinggy.lib"); // <- path to pinggy.lib
 
 // Ensure lib folder exists
 fs.mkdirSync(path.dirname(dest), { recursive: true });
@@ -23,16 +21,9 @@ fs.rmSync(path.join(__dirname, "build"), { recursive: true, force: true });
 console.log(`Removed build/ directory`);
 
 // Remove pinggy.lib if it exists
-if (fs.existsSync(libPathWin)) {
-  fs.rmSync(libPathWin);
+if (fs.existsSync(libPath)) {
+  fs.rmSync(libPath);
   console.log(`Removed pinggy.lib`);
-} else if (fs.existsSync(libPathMac)) {
-  fs.rmSync(libPathMac);
-  console.log(`Removed libpinggy.dylib`);
-}
-else if (fs.existsSync(libPathLinux)) {
-  fs.rmSync(libPathLinux);
-  console.log(`Removed libpinggy.so`);
 } else {
-  console.warn(`pinggy.lib / libpinggy.dylib / libpinggy.so not found`);
+  console.warn(`pinggy.lib not found in root directory`);
 }
